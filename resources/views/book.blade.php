84145ef5f7518cdd881dfd74e8e576c61faa8afc
tegar2804 <muhammadtegar2804@gmail.com>
2023-05-09 17:50:35 +0700

    rating

--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -2,13 +2,17 @@
 
 @section('container')
     <h1>Book!!!</h1>
-    {{ dd(auth()->user()->book->find(1)->get()) }}
-    @if(auth()->user()->id == $book->author->id)
-        <a href=""><h4>Edit</h4></a>
-        <a href=""><h4>Delete</h4></a>
-    @elseif(auth()->user()->book->find($book->author->id))
-        <a href=""><h4>keranjang</h4></a>
-    @endif
+    @auth
+        @if(auth()->user()->book->find($book->id))
+            {{ auth()->user()->book->find($book->id)->collection[0]->rate }}
+        @endif
+        @if(auth()->user()->id == $book->author->id)
+            <a href=""><h4>Edit</h4></a>
+            <a href=""><h4>Delete</h4></a>
+        @elseif(!auth()->user()->book->find($book->id))
+            <a href=""><h4>keranjang</h4></a>
+        @endif
+    @endauth
     <h2>{{ $book->title }}</h2>
     <img style="border: 1px solid #ddd; border-radius: 4px; padding: 5px; width: 450px;" src="{{ asset('storage/'. $book->cover) }}" alt="cover buku '{{ $book->title }}'">
     <h3>isbn: {{ $book->isbn }}</h3>
@@ -21,4 +25,11 @@
     @endforeach
     <h3>desc: </h3>
     <p>{{ $book->desc }}</p>
+    @auth
+        @if(auth()->user()->book->find($book->id))
+            <a href=""><h4>Rating</h4></a>
+        @else
+            <h4>Rating</h4>
+        @endif
+    @endauth
 @endsection
