2e51ec9efc9c8043bd3ef1ea591cfa87304a9a16
tegar2804 <muhammadtegar2804@gmail.com>
2023-05-09 07:09:58 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\UploadController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\RegisterController;
-use App\Http\Controllers\CollectionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,29 +13,36 @@ use App\Http\Controllers\CollectionController;
 |
 */
 
-Route::get('/', [BookController::class, 'index']);
-Route::get('/book', [BookController::class, 'all']);
-Route::get('/book/{book:slug}', [BookController::class, 'show']);
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{ 
+    Route::get('/', 'BookController@index');
 
-Route::get('/categories', [CategoryController::class, 'index']);
-// Route::get('/categories/{category:slug}', [CategoryController::class, 'show']);
+    Route::get('/book', 'BookController@all');
+    Route::get('/book/{book:slug}', 'BookController@show');
 
-Route::get('/collection', [CollectionController::class, 'index']);
+    Route::get('/categories', 'CategoryController@index');
 
-//cart
+    Route::group(['middleware' => ['guest']], function() {
+        Route::get('/login', 'LoginController@index')->name('login');
+        Route::post('/login', 'LoginController@login');
+        Route::get('/register', 'RegisterController@index');
+        Route::post('/register', 'RegisterController@regis');
+        //forgot-pass
+    });
 
-//history
-//history/invoice
+    Route::group(['middleware' => ['auth']], function() {
+        Route::post('/logout', 'LoginController@logout');
 
-Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
-Route::post('/login', [LoginController::class, 'login']);
-Route::post('/logout', [LoginController::class, 'logout']);
+        Route::get('/profile', 'UserController@index');
 
-Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
-Route::post('/register', [RegisterController::class, 'regis']);
+        Route::get('/collection', 'CollectionController@index');
 
-Route::get('/profile', [UserController::class, 'index'])->middleware('auth');
+        Route::get('/upload/checkSlug', 'UploadController@checkSlug');
+        Route::resource('/upload', 'UploadController');
 
-Route::get('/upload/checkSlug', [UploadController::class, 'checkSlug'])->middleware('auth');
-Route::resource('/upload', UploadController::class)->middleware('auth');
-//forgot-pass
+        //cart
+
+        //history
+        //history/invoice
+    });
+});
